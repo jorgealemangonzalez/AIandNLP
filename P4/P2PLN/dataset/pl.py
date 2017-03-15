034c3cd857d2c9ef2ae8c9acf20b9d94566a05c3
u114188 <u114188@DS.UPF.EDU>
2017-03-15 17:12:07 +0100

    have to test it

--- P4/P2PLN/dataset/pl.py
+++ P4/P2PLN/dataset/pl.py
@@ -1,16 +1,8 @@
 import nltk , glob 
 import sys
 
-
-class SearchMostUsed: 
-	WordsCount = {}
-	for filename in glob.glob('*_*'):
-		fp = open(filename,"r")
-		for word in fp.read().split():
-   			WordsCount[(word)] = 1
-   		#for i in range(len(WordsCount)):
-   		#	print(WordsCount[i])
-   		#close(fp)
-   		for i in range(len(WordsCount)) :
-   			print(WordsCount[i])
-   		#close(fp)
+class SearchMostUsed:
+    for filename in glob.glob('*_*'):
+        fp = open(filename,"r")
+        fdist = FreqDist(fp)
+        print(fdist)
